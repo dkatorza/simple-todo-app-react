75b33f30b4e8313ef2c64a51f2cc15e0503c129e
Dan Katorza <dkatorza@gmail.com>
2021-12-09 15:07:16 +0200

    1.Adding dummie data + styling.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,26 @@
 function App() {
   return (
-    <div className="App">
-      <h1>Hello World</h1>
+    <div className="app-general">
+      <div className="logo">
+        <h1>Toodlidoo</h1>
+      </div>
+      <div className="title">
+        <h4>Your Tasks</h4>
+      </div>
+
+      <div className="todos">
+        <div className="todo">
+          <div className="checkbox"></div>
+          <div className="text">Dummy todo</div>
+          <div className="delete-todo">x</div>
+        </div>
+        <div className="todo completed">
+          <div className="checkbox"></div>
+          <div className="text">Dummy todo 2</div>
+          <div className="delete-todo">x</div>
+        </div>
+      </div>
+
     </div>
   );
 }
